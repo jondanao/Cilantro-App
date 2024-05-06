56e7668609d058dcd6871ceb37750f784e4b2f2f
Jon Raphael Danao <jon@theappfactory.io>
2024-05-06 22:18:45 +0800

    wip: render title

--- app/features/recipes/RecipeScreen.tsx
+++ app/features/recipes/RecipeScreen.tsx
@@ -12,6 +12,10 @@ const RecipeScreen = ({ route, navigation }) => {
         <View style={styles.container}>
             <ScrollView>
                 <Image source={{ uri: recipe.image }} style={styles.image} />
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>{recipe.label}</Text>
+                    <Text style={styles.source}>{recipe.source}</Text>
+                </View>
             </ScrollView>
         </View>
     );
@@ -22,9 +26,24 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "#fff",
     },
+
+    titleContainer: {
+        padding: 20,
+    },
+
     image: {
         width: "100%",
         height: 300,
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+
+    source: {
+        fontSize: 18,
+        color: "gray",
     },
 });
 
